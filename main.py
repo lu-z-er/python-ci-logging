ead2ec9d2035e34be0eec7e6303634881a20f120
lu-z-er <spreet2003@gmail.com>
2025-04-21 17:12:47 +0530

    Initial Commit!!!

---
+++ main.py
@@ -0,0 +1,11 @@
+import logging
+logging.basicConfig(filename='app.log', level=logging.INFO)
+def main():
+    logging.info("Script started.")
+    try:
+        x = 10 / 0
+    except ZeroDivisionError as e:
+        logging.error(f"Error occurred: {e}")
+        raise e
+if __name__ == "_main_":
+    main()
